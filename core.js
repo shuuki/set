4cc9965d4bb6c51bbd0178b827aae39cfd838f47
Max Rubenacker <shuuki@gmail.com>
2017-05-01 23:41:28 -0600

    fixed script integration

--- core.js
+++ core.js
@@ -1,7 +1,9 @@
 console.log('ready')
 
 
+var body = document.getElementsByTagName("body")[0];
+
 var button = document.createElement("button");
 button.innerHTML = "Begin";
 body.appendChild(button);
-button.addEventListener("click", init);
+button.addEventListener("click", function() {});
