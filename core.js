7aa120ee2651597f227e948d2a5e7acaf6a46517
Max Rubenacker <shuuki@gmail.com>
2017-05-02 15:31:50 -0600

    added auto save/load, separating functions

--- core.js
+++ core.js
@@ -2,52 +2,64 @@ console.log('ready')
 
 // globals
 
-var up = 0, time = 0, running = false, luminosity;
+var up = 0,
+	time = 0,
+	running = false,
+	luminosity,
+	cache = {};
+
 
 // elements
 
-var body = document.getElementsByTagName("body")[0];
+var body = document.getElementsByTagName('body')[0];
 
-var button = document.createElement("button");
-button.innerHTML = "Begin";
-button.addEventListener("click", init);
+var playpause = document.createElement('button');
+playpause.innerHTML = 'Begin';
+playpause.addEventListener('click', init);
 
-var div = document.createElement("div");
+var forget = document.createElement('button');
+forget.innerHTML = 'Reset';
+forget.addEventListener('click', reset);
 
-var pre = document.createElement("pre");
 
-body.appendChild(button);
+var div = document.createElement('div');
+
+var pre = document.createElement('pre');
+
+body.appendChild(playpause);
+body.appendChild(forget);
 body.appendChild(div);
 body.appendChild(pre);
-
-
 
 
 
 // steps
 
 function init() {
-	
 	time = Date.now();
-	
 	if (!running) {
 		window.requestAnimationFrame(update)    
 		running = true;
-		button.innerHTML = "Pause";
+		playpause.innerHTML = 'Pause';
 	} else {
 		window.cancelAnimationFrame(update)
 		running = false;
-		button.innerHTML = "Resume";
+		playpause.innerHTML = 'Resume';
 	}
-	
-	
+	load()
 }
 
 function update() {
-  if (running)
-    window.requestAnimationFrame(update)
+  if (running) window.requestAnimationFrame(update)
+	inputs()
 	tick()
 	render()
+	save()
+}
+
+
+function inputs() {
+	
 }
 
 function tick() {
@@ -70,38 +82,74 @@ function clocks(time) {
 	return clock;
 }
 
+
+function beats(time) {
+	var beat = [];
+	
+		beat[0] = ((Math.sin(time / 500)+1)/2).toFixed(3),
+		beat[1] = ((Math.sin(time / 125)+1)/2).toFixed(3),
+		beat[2] = ((Math.sin(time / 2000)+1)/2).toFixed(3);
+		return beat;
+}
+
+
 function render() {
 
 	var c = clocks(up);
+	var b = beats(up);
 
-	var beat1 = ((Math.sin(up / 500)+1)/2).toFixed(3),
-    beat2 = ((Math.sin(up / 125)+1)/2).toFixed(3),
-    beat3 = ((Math.sin(up / 2000)+1)/2).toFixed(3);
-
-
+	
 	div.innerHTML = 
 		p('Running: '+running) 
-		+ " " 
+		+ ' ' 
 		//+ p('Luminosity: '+luminosity)
 		+ p('Uptime: ' + up)
 		+ p(c.hr + ' hr : ' + c.min + ' min : ' + c.sec + ' sec')
-		+ p(beat1) 
-		+ p(beat2) 
-		+ p(beat3);
+		+ p(b[0]) 
+		+ p(b[1]) 
+		+ p(b[2]);
 		
 		// 28 days beat: 28*24*60*60*1000
 
-  pre.innerHTML = linegen(beat1,beat2,beat3);
+  pre.innerHTML = linegen(b[0], b[1], b[2]);
 
 }
 
 function p(content) {
-	return "<p>" + content + "</p>";
+	return '<p>' + content + '</p>';
 }
+
+
+function save() {
+	cache = { 
+		time: up
+	};
+	window.localStorage.setItem('SETPOINT', JSON.stringify(cache));
+}
+
+function load() {
+	if (localStorage.getItem('SETPOINT')) {
+		cache = JSON.parse(localStorage.getItem('SETPOINT'));
+		up = cache.time
+	}
+}
+
+function reset() {
+	if (localStorage.getItem('SETPOINT')) {
+		localStorage.removeItem('SETPOINT');
+		up = 0
+	}
+}
+
 
 //function roll() {}
 
 init();
+
+
+
+
+
 
 
 
@@ -143,15 +191,15 @@ s.stop();
 
 */
 
-window.addEventListener("devicelight", function (event) {
+window.addEventListener('devicelight', function (event) {
 	luminosity = event.value;
 	console.log(luminosity)
 	//event.value is the lux value returned by the sensor on the device
 	if (event.value < 100) {
-		document.body.style.backgroundColor="#777";
-		document.body.style.color="#fff";
+		document.body.style.backgroundColor='#777';
+		document.body.style.color='#fff';
 	} else {
-		document.body.style.backgroundColor="#fff";
+		document.body.style.backgroundColor='#fff';
 	}
 
 });
@@ -174,11 +222,11 @@ scene.update = {}
 scene.draw = {}
 
 scene.updates = () => {
-	if (typeof scene.update[scene.active] === "function" ) scene.update[scene.active]()
+	if (typeof scene.update[scene.active] === 'function' ) scene.update[scene.active]()
 }
 
 scene.drawing = () => {
-	if (typeof scene.draw[scene.active] === "function" ) scene.draw[scene.active]()
+	if (typeof scene.draw[scene.active] === 'function' ) scene.draw[scene.active]()
 }
 
 scene.reset = () => {
